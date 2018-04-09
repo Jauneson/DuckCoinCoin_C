8397bbc35509fc3ee09a9b9d8eb678407cd1e1e9
NoSylhex <36363833+NoSylhex@users.noreply.github.com>
2018-04-09 16:31:36 +0200

    Transaction.h

---
+++ Transaction.h
@@ -0,0 +1,60 @@
+#ifndef __TRANSACTION_H__
+#define __TRANSACTION_H__
+
+#define MAX_VALUE 10 
+/**
+ * @Transaction structure 
+ */
+typedef struct s_transactionDeque *TransactionDeque ;
+/**
+ * @ 
+ */
+Transaction * create_transaction() ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @return [description]
+ */
+ TransactionDeque init_transaction_deque() ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param transaction [description]
+ * @return [description]
+ */
+void set_hash_transaction(Transaction  transaction) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param t [description]
+ */
+void add_transaction_to_transactionDeque(TransactionDeque t) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param transaction [description]
+ */
+void remove_transaction(Transaction transaction) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param t [description]
+ */
+void delete_transaction_deque(TransactionDeque t) ;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param transaction [description]
+ * @return [description]
+ */
+int get_index(Transaction transaction) ;
+
+#endif
+
+
